a155c8b914f95e22ec6132cfc4cc05f9180782b1
Caitlin Ostroff <caitlin.ostroff@gmail.com>
2024-11-15 16:16:51 -0500

    Update inmates.py

--- inmates.py
+++ inmates.py
@@ -1,115 +1,73 @@
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-import re
-import time
 import csv
+import requests
+from bs4 import BeautifulSoup
+import time
+import re
 
-# function to get the links for the first page that uses html
-def getLinks(pageVals):
-    global driver
-    #go down the page to the section with this class
-    pageSection = driver.find_element_by_class_name("dcCSStableLight")
-    # find the a tags within a specific table
-    pageNav = pageSection.find_elements_by_xpath("/html/body/div/div/div/table/tbody/tr/td/table/tbody/tr/td/a")
-    #for each a tag among these a tags
-    for pageLink in pageNav:
-        #get the href (the url) and add it to the list called pagesVals
-        pageVals.append(pageLink.get_attribute('href'))   
-        
-#function to get the links for all pages after the first        
-def getPages(pageVals):
-    global driver
-    #variable instructs the driver to find the button on the page
-    button = driver.find_element_by_xpath("//input[@name='rsOffenderRoot_PagingMove' and @value='Next']")
-    #while the button exists, run a loop. this will keep running until the button no longer exists
+def get_employee_data():
+    employee_data = []
+    base_url = "https://directory.psc.gov/hhsdir/eeKey.asp?Key="
+    page_no = 0
+    existing_page_numbers = set()  
+
+    try:
+        with open('hhs_employee_directory.csv', 'r', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                try:
+                    existing_page_numbers.add(int(row['page_no'])) 
+                except (KeyError, ValueError):
+                    pass 
+    except FileNotFoundError:
+        pass
+
     while True:
         try:
-            #wait for the page to load and after a certain amount of time has passed, find the button
-            button = WebDriverWait(driver, 5, 0.25).until(EC.visibility_of_element_located([By.XPATH, "//input[@name='rsOffenderRoot_PagingMove' and @value='Next']"]))
-            #click the button
-            button.click()
-            #take a break. even scrapers get tired
-            time.sleep(3)
-            #same thing from get links: find the part of the page with this class
-            pageSection = driver.find_element_by_class_name("dcCSStableLight")
-            #find the a tags in the table
-            pageNav = pageSection.find_elements_by_xpath("/html/body/div/div/div/table/tbody/tr/td/table/tbody/tr/td/a")
-            #for each page, get all of the a tags
-            for pageLink in pageNav:
-                #for each of those links, get the href and add it to the same list called pageVals
-                pageVals.append(pageLink.get_attribute('href'))
-            #run this function over and over, doing this for each page UNLESS it no longer works, in which case break the loop
-        except:
-            break
-    
-        
-#function to get the demographic information for each inmate    
-def getDetails(pageVals):
-    global driver
-    global inmates
-    #for each link in the list you have stored in pageVals
-    for value in pageVals:
-        #open each link
-        html = urlopen(value)
-        #we're using BeautifulSoup to scrape
-        bsObj = BeautifulSoup(html, "html.parser")
-        #find all of the td tags within a table 
-        td_list = bsObj.findAll("td", {"align":"LEFT"})
-        #make a python dictionary, assigning a key and for each key, an attribute. Because these tags make have weird spacing, get their text and strip all formatting. This makes it easier to use when we export to a CSV
-        inmate = {
-            'id': td_list[0].get_text().strip(),
-            'name': td_list[1].get_text().strip(),
-            'race': td_list[2].get_text().strip(),
-            'sex': td_list[3].get_text().strip(),
-            'dob': td_list[9].get_text().strip(),
-            'entry': td_list[10].get_text().strip(),
-            'facility': td_list[11].get_text().strip(),
-            'custody': td_list[12].get_text().strip(),
-            'release': td_list[13].get_text().strip(),            
-        }
-        #make a new python list called inmates and append your python dictionary, inmate, to that
-        inmates.append(inmate)
-        
-#function to save all of your hard work to a CSV        
-def saveToCSV(inmates):
-    global driver
-    #give the csv file you want to export it to a name
-    filename = 'inmates.csv' 
-    #open your new csv file with a 'w' so you can write to it
-    with open(filename, 'w') as output_file:
-        #make headers for you columns. these must match up with the keys you set in your python dictionary, inamte
-        fieldnames = [	'id',
-                      'name',
-                      'race',
-                      'sex',
-                      'dob',
-                      'entry',
-                      'facility',
-                      'custody',
-                      'release',
-                     ]
-        #write these into a csv, the headers being fieldnames and the rows your list of inmates
+            page_no += 1
+            print(f"Checking page: {page_no}")
+            if page_no in existing_page_numbers:
+                print(f"Page {page_no} already exists. Skipping.")
+                continue  
+
+            full_url = base_url + str(page_no)
+            r = requests.get(full_url, timeout=10)  
+            r.raise_for_status()  
+            time.sleep(1)
+            soup = BeautifulSoup(r.content, 'html.parser')
+
+            data_elements = {}
+            for label in ["Last name", "First name", "Agency", "Organization", "Job title", "E-mail"]:
+                try:
+                    element = soup.find(string=re.compile(label)).find_next()
+                    data_elements[label.lower().replace(" ", "_")] = element.text.strip()
+                except AttributeError:
+                    data_elements[label.lower().replace(" ", "_")] = ""
+
+            data_elements['page_no'] = page_no 
+
+            employee_data.append(data_elements)
+            print(data_elements)
+
+        except requests.exceptions.RequestException as e:
+            print(f"Network error on page {page_no}: {e}")
+            break 
+        except (AttributeError, Exception) as e:
+            print(f"Error processing page {page_no}: {e}")
+            break  
+
+
+    return employee_data
+
+
+def save_data(employee_data):
+    filename = 'hhs_employee_directory.csv'
+    fieldnames = ['page_no', 'last_name', 'first_name', 'agency', 'organization', 'job_title', 'email']
+    with open(filename, 'w', newline='', encoding='utf-8') as output_file:  #Add newline='' and encoding
         writer = csv.DictWriter(output_file, fieldnames=fieldnames)
         writer.writeheader()
-        writer.writerows(inmates)
-#you prefered driver has to open the original page to do all this. I use Firefox here but Chrome is also an option        
-driver = webdriver.Firefox()
-driver.get("http://www.dc.state.fl.us/activeinmates/list.asp?dataaction=GetInst&facility2=279")
-#your lists where info will be stored
-pageVals = []
-inmates = []
-#run the functions, using the values (links) of the lists we created        
-getLinks(pageVals) 
-getPages(pageVals) 
-driver.close() # close the driver 
-
-getDetails(pageVals)
-saveToCSV(inmates)
+        writer.writerows(employee_data)
 
 
-#Ta-Da!    
+if __name__ == "__main__":
+    employee_data = get_employee_data()
+    save_data(employee_data)
